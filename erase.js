f96164497cc00da44c68fbb87f779acd269c5168
Baruch Velez <baruchvelez@gmail.com>
2016-03-01 16:45:17 -0600

    more fucking things

--- erase.js
+++ erase.js
@@ -1,17 +1,16 @@
-var thinky = require('thinky')
-
-var opts = {
+var thinky, opts, db, r, thing;
+thinky = require('thinky')
+opts = {
   db : "todo_app_LOCAL",
   host : "",
   port : "",
   authKey : "uRf77UnjvzhuoAI0t4ZMnvmFG6609XQKbCkFJNe8GQ"
 }
-var db = thinky(opts)
-var r = db.r
-var thing = function(){
+db = thinky(opts)
+r = db.r
+thing = function(){
   r.table('User').delete().run().then(function(){
     console.log("Delete Happen")
   })
 }
-
 thing()
